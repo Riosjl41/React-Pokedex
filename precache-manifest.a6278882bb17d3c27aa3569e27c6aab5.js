1caaefc04725a4ecaa547df115b891aa7104f434
jlrios41 <Jlrios@nevada.unr.edu>
2019-03-12 20:57:40 -0700

    Updates

---
+++ precache-manifest.a6278882bb17d3c27aa3569e27c6aab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91791c21b2a6fc4e0355",
+    "url": "/React-Pokedex/static/js/runtime~main.91791c21.js"
+  },
+  {
+    "revision": "2d3de0becbe7fc802d4c",
+    "url": "/React-Pokedex/static/js/main.2d3de0be.chunk.js"
+  },
+  {
+    "revision": "0af4d90a369e81b79e79",
+    "url": "/React-Pokedex/static/js/1.0af4d90a.chunk.js"
+  },
+  {
+    "revision": "2d3de0becbe7fc802d4c",
+    "url": "/React-Pokedex/static/css/main.7c5f1121.chunk.css"
+  },
+  {
+    "revision": "0af4d90a369e81b79e79",
+    "url": "/React-Pokedex/static/css/1.38ea55c9.chunk.css"
+  },
+  {
+    "revision": "87c946daff50fd0b3f68795c85474d79",
+    "url": "/React-Pokedex/index.html"
+  }
+];
